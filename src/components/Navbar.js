b13a34b25cad94dfa268114fc43136bff4640c2a
Abdullah <abdullahpoi@gmail.com>
2023-10-08 03:09:16 +0530

    ...

---
+++ src/components/Navbar.js
@@ -0,0 +1,87 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  {/* Container wrapper */}
+  <div className="container">
+    {/* Navbar brand */}
+    <a className="navbar-brand me-2" href="https://mdbgo.com/">
+      <img
+        src="Screenshot 2023-07-29 022710.png"
+        height={80}
+        alt="MDB Logo"
+        loading="lazy"
+        style={{ marginTop: "-1px" }}
+      />
+    </a>
+    {/* Toggle button */}
+    <button
+      className="navbar-toggler"
+      type="button"
+      data-mdb-toggle="collapse"
+      data-mdb-target="#navbarButtonsExample"
+      aria-controls="navbarButtonsExample"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <i className="fas fa-bars" />
+    </button>
+    {/* Collapsible wrapper */}
+    <div className="collapse navbar-collapse" id="navbarButtonsExample">
+      {/* Left links */}
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            Home
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            Types of Issues
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            Counselling Services
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            Session &amp; Fees
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            Blog
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            Contact
+          </a>
+        </li>
+      </ul>
+      {/* Left links */}
+      <div className="d-flex align-items-center">
+        <button type="button" className="btn btn-link px-3 me-2">
+          <i className="fa fa-phone" aria-hidden="true">
+            +919580477026
+          </i>
+        </button>
+        <button type="button" className="btn btn-primary me-3">
+          Book an Appointment
+        </button>
+      </div>
+    </div>
+    {/* Collapsible wrapper */}
+  </div>
+  {/* Container wrapper */}
+</nav>
+
+    </div>
+  )
+}
+
+export default Navbar
